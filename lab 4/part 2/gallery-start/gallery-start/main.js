bad9ea9f41b26a58db2b05558b0d8fba001f566a
Hayden-heaslip <hayden.heaslip@dcmail.ca>
2024-04-03 15:31:12 -0400

    pics almost working

--- lab 4/part 2/gallery-start/gallery-start/main.js
+++ lab 4/part 2/gallery-start/gallery-start/main.js
@@ -6,13 +6,17 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
 /* Declaring the alternative text for each image file */
 
+const alter = ["eye close up", "weird wave picture",  "flowers", "Egyptian gods", "butterfly"];
 /* Looping through images */
-
+for (let i = 0; i < images.length; i++)
+{
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', images[i]);
+newImage.setAttribute('alt', alter[i]);
 thumbBar.appendChild(newImage);
-
+}
 /* Wiring up the Darken/Lighten button */
