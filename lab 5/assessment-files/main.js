7bd433d6f52ec358081d42c6469caf2dce90c641
Hayden-heaslip <hayden.heaslip@dcmail.ca>
2024-04-12 14:41:17 -0400

    finished the project

--- lab 5/assessment-files/main.js
+++ lab 5/assessment-files/main.js
@@ -16,6 +16,20 @@ showHideBtn.onclick = function() {
   }
 };
 
+document.addEventListener('keypress', (event)=>{
+     let keyCode = event.keyCode ? event.keyCode : event.which;
+     if(keyCode === 13) {
+let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }     }
+       
+   });
+
 // functionality for adding a new comment via the comments form
 
 const form = document.querySelector('.comment-form');
